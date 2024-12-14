d2f8c63f9ee6813bd24405902df2b29e29bfc9d3
CnegAsuy <81309318+CnegAsuy@users.noreply.github.com>
2024-12-14 16:06:31 +0300

    somethings with RefCells

---
+++ src/fractional.rs
@@ -0,0 +1,24 @@
+use std::f64::consts::PI;
+
+fn main() {
+    print!("{}", make_it_fractional(PI));
+}
+
+fn make_it_fractional(num: f64) -> String {
+    let mut counter = 0.;
+    let rnum = {
+        loop {
+            counter += 1.;
+            if is_natural_number(num * counter) {
+                break;
+            }
+        }
+        num * counter
+    };
+    format!("{rnum}/{counter}")
+}
+
+fn is_natural_number(n: f64) -> bool {
+    n > 0.0 && n.fract() == 0.0
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
-use std::f64::consts::PI;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
-    print!("{}", make_it_fractional(1425.));
-}
+    let shared_string = Rc::new(RefCell::new(String::from("Hello")));
 
-fn make_it_fractional(num: f64) -> String {
-    let mut counter = 0.;
-    let rnum = {
-        loop {
-            counter += 1.;
-            if is_natural_number(num * counter) {
-                break;
-            }
-        }
-        num * counter
-    };
-    format!("{rnum}/{counter}")
+    let reference = Rc::clone(&shared_string);
+    
+    {
+        reference.borrow_mut().push_str(", world!");    
+    }
+    println!("Reference 2: {}", shared_string.borrow());
 }
-
-fn is_natural_number(n: f64) -> bool {
-    n > 0.0 && n.fract() == 0.0
-}
-
